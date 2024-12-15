29bee17b9f4155f9b0ee111621f9026cf8b2fde3
Junhee Lee <jheelee020421@gmail.com>
2024-12-15 03:28:32 -0500

    creating panels dynamically

--- src/script.js
+++ src/script.js
@@ -1,13 +1,47 @@
-const panels = document.querySelectorAll(".panel");
+function fetchJSONData() {
+    fetch("./photos.json")
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            createPanelElement(data);            
+        })
+        .catch((error) =>
+            console.error("Unable to fetch data:", error)
+        );
+}
 
-panels.forEach((panel) => {
-    panel.addEventListener("click", () => {
-        removeActiveClasses();
-        panel.classList.add("active");
+function createPanelElement(data) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    document.body.appendChild(container);
+
+    data.forEach((photo) => {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("panel");
+
+        newDiv.style.backgroundImage = `url(${photo.src})`;
+
+        const h1 = document.createElement("h1");
+        h1.textContent = photo.title;
+        newDiv.appendChild(h1);
+
+        newDiv.addEventListener("click", () => {
+            removeActiveClasses(container);
+            newDiv.classList.add("active");
+        });
+
+        container.appendChild(newDiv);
     });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchJSONData();
 });
 
-function removeActiveClasses() {
+function removeActiveClasses(container) {
+    const panels = container.querySelectorAll(".panel");
     panels.forEach((panel) => {
         panel.classList.remove("active");
     });
